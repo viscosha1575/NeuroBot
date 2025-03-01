75bb3ca0d7defa7d233600b3977f24fd7d363d67
viscosha1575 <61380433+viscosha1575@users.noreply.github.com>
2025-03-01 13:12:09 +0100

    п

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -1,23 +1,33 @@
 const TelegramBot = require('node-telegram-bot-api');
-
 require('dotenv').config();
+const express = require('express');
+
 const TOKEN = process.env.TOKEN;
+const PORT = process.env.PORT || 3000;
+
 const bot = new TelegramBot(TOKEN, { polling: true });
-const webAppUrl = 'https://eggsgame-6b328.web.app/'
+const app = express();
+
+const webAppUrl = 'https://eggsgame-6b328.web.app/';
+
+// Маршрут для проверки работы сервера
+app.get('/', (req, res) => {
+    res.send('Бот работает!');
+});
 
 // Обработчик команды /start
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
     await bot.sendMessage(chatId, `Привет, ${msg.from.first_name}! Я твой Telegram-бот.`);
-    
+
     // Отправляем сообщение с inline-кнопкой для открытия мини-приложения
     await bot.sendMessage(chatId, 'Начните игру', {
         reply_markup: {
             inline_keyboard: [
                 [
                     {
-                        text: 'Открыть игру', // Текст кнопки
-                        web_app: { url: webAppUrl, request_full_screen: true }, // Ссылка на мини-приложение
+                        text: 'Открыть игру',
+                        web_app: { url: webAppUrl, request_full_screen: true },
                     },
                 ],
             ],
@@ -38,5 +48,8 @@ bot.on('message', (msg) => {
     }
 });
 
-console.log('Бот запущен...');
+// Запуск Express-сервера (нужен для Render)
+app.listen(PORT, () => {
+    console.log(`Бот запущен на порту ${PORT}`);
+});
 
